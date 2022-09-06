f71048e0b26bd6716e5291da1344d9bbbc40aa21
yogeshium <112271449+yogeshium@users.noreply.github.com>
2022-09-06 23:41:45 +0530

    Commit 07-Sept (+ Aproach )

--- Leetcode_Gfg/Merge_Sorted_Array.cpp
+++ Leetcode_Gfg/Merge_Sorted_Array.cpp
@@ -1,6 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+   void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1,j=n-1,k=m+n-1;
+        while(i>=0&&j>=0)
+        {
+            if(nums1[i]>nums2[j])
+            {
+                nums1[k]=nums1[i];
+                i--;
+                k--;
+            }
+            else
+            {
+                nums1[k]=nums2[j];
+                j--;
+                k--;
+            }
+        }
+        while(i>=0)
+            nums1[k--]=nums1[i--];
+        while(j>=0)
+            nums1[k--]=nums2[j--];
+    }
+
+*/
+
+
 int main()
 {
     vector<int>nums1 = {1,2,5,0,0,0};
